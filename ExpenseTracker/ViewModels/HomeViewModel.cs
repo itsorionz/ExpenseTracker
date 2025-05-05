5837eb384fe76a1f2542a9a0249f9fac6fac3aba
iamAnamul Haque Sohel <anamul.sohel97@outlook.com>
2025-05-05 19:24:50 +0600

    home page design and logic change

--- ExpenseTracker/ViewModels/HomeViewModel.cs
+++ ExpenseTracker/ViewModels/HomeViewModel.cs
@@ -28,7 +28,7 @@ namespace ExpenseTracker.ViewModels
             var data = await _db.GetTransactionsAsync();
             Transactions.Clear();
 
-            foreach (var t in data.OrderByDescending(t => t.Date))
+            foreach (var t in data.OrderByDescending(t => t.Date).Take(8))
                 Transactions.Add(t);
 
             TotalBalance = Transactions.Sum(t => t.Type == "Income" ? t.Amount : -t.Amount);
